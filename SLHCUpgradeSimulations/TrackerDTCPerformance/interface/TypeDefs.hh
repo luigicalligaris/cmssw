4abda9da33af933bd0ff4ec65b23a64eda9d1739
Luigi <luigi.calligaris@cern.ch>
2018-01-15 16:59:21 -0200

    Remove useless Typedefs.hh

--- SLHCUpgradeSimulations/TrackerDTCPerformance/interface/TypeDefs.hh
+++
@@ -1,8 +0,0 @@
-#ifndef SLHCUpgradeSimulations_TrackerDTCPerformance_Typedefs_hh
-#define SLHCUpgradeSimulations_TrackerDTCPerformance_Typedefs_hh
-
-#include "DataFormats/DetId/interface/DetId.h"
-
-#include <functional>
-
-#endif // end SLHCUpgradeSimulations_TrackerDTCPerformance_Typedefs_hh
